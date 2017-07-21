3bb7d52c37fc5550b578b403a39bd33d095c8a83
amalakamala <amalakamala.a@gmail.com>
2017-07-20 22:05:39 -0400

    maqueta header

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$(".button-collapse").sideNav();
+	/*Dropdown*/
+	$(".dropdown-button").dropdown();
+	/*Carrusel*/
+	$('.carousel.carousel-slider').carousel({fullWidth: true});
+
+	$('.carousel').carousel();
+		setInterval(function() {
+		$('.carousel').carousel('next');
+	}, 3000); 
+});
